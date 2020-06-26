0f7959848aa143a0b1a2c9177fb4354f05890afb
杨腾祺 <3150475761@qq.com>
2020-06-26 20:30:32 +0800

    08-注册功能完成

--- src/views/RegisterPage.vue
+++ src/views/RegisterPage.vue
@@ -7,6 +7,7 @@
       inputText="用户名/手机号码"
       rule="^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\d{8}$"
       errMsg="手机号码或用户名格式不正确"
+      @leaved="setUser"
     />
     <InputSec
       type="text"
@@ -14,6 +15,7 @@
       inputText="昵称"
       rule="^[\u4E00-\u9FA5A-Za-z0-9_]+$"
       errMsg="昵称格式不正确"
+      @leaved="setNick"
     />
     <InputSec
       type="password"
@@ -21,9 +23,10 @@
       inputText="密码"
       rule="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^]{8,16}$"
       errMsg="密码格式不正确"
+      @leaved="setPwd"
     />
     <MethodSec />
-    <LoginSec logInfo="注册" />
+    <LoginSec logInfo="注册" @send="sendData" />
   </div>
 </template>
 
@@ -33,6 +36,43 @@ import InputSec from "@/components/InputSec";
 import MethodSec from "@/components/MethodSec";
 import LoginSec from "@/components/LoginSec";
 export default {
+  data() {
+    return {
+      newUser: "",
+      newNick: "",
+      newPwd: ""
+    };
+  },
+  methods: {
+    setUser(newVal) {
+      this.newUser = newVal;
+    },
+    setNick(newVal) {
+      this.newNick = newVal;
+    },
+    setPwd(newVal) {
+      this.newPwd = newVal;
+    },
+    sendData() {
+      this.$axios({
+        url: "http://127.0.0.1:3000/register",
+        method: "post",
+        data: {
+          username: this.newUser,
+          password: this.newPwd,
+          nickname: this.newNick
+        }
+      }).then(res => {
+        const { message } = res.data;
+        if (message == "注册成功") {
+          this.$toast.success(message);
+          setTimeout(() => {
+            this.$router.push("/login");
+          }, 500);
+        }
+      });
+    }
+  },
   components: {
     CommonSec,
     InputSec,
